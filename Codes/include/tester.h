03c1dcdc44474e2b91eaaf8ce7be42e516a273ff
inmove <lisper.inmove@gmail.com>
2026-04-05 17:44:06 +0800

    contours functions

--- Codes/include/tester.h
+++ Codes/include/tester.h
@@ -14,6 +14,9 @@ public:
 protected:
   std::string assets_ = "/home/inmove/nvme1/Learn/LearnOpenCV/Codes/assets/";
   std::string filepath_ = std::filesystem::path(assets_) / "bee.png";
+  std::string cxPath_ = std::filesystem::path(assets_) / "xt.png";
+  std::string stuffPath_ = std::filesystem::path(assets_) / "stuff.jpg";
+  std::string contoursPath_ = std::filesystem::path(assets_) / "contours.png";
   std::string ricePath_ = std::filesystem::path(assets_) / "rice.png";
   std::string savedFilepath_ = std::filesystem::path(assets_) / "bee2.png";
   std::string videoPath_ = std::filesystem::path(assets_) / "green_light.mp4";
