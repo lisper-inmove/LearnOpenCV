c794a3573696990d00f1eef2026aab67be6d5bc7
inmove <lisper.inmove@gmail.com>
2026-04-05 14:03:47 +0800

    CCL: connected component labeling

--- Codes/include/tester.h
+++ Codes/include/tester.h
@@ -14,6 +14,7 @@ public:
 protected:
   std::string assets_ = "/home/inmove/nvme1/Learn/LearnOpenCV/Codes/assets/";
   std::string filepath_ = std::filesystem::path(assets_) / "bee.png";
+  std::string ricePath_ = std::filesystem::path(assets_) / "rice.png";
   std::string savedFilepath_ = std::filesystem::path(assets_) / "bee2.png";
   std::string videoPath_ = std::filesystem::path(assets_) / "green_light.mp4";
   std::string jyNoisePath_ = std::filesystem::path(assets_) / "jy_noise.png";
